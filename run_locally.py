d700b87e79173087dfa66d337a2211143c10bac7
Ruoyu Sun <ruoyu@unity3d.com>
2019-01-15 13:15:16 +0200

    Not to produce too many roaches

--- run_locally.py
+++ run_locally.py
@@ -1,5 +1,6 @@
 import json
 
+from examples.zerg.zerg_rush import ZergRushBot
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
@@ -14,7 +15,7 @@ def main():
 
     run_game(maps.get("(2)DreamcatcherLE"), [
         Bot(race, MyBot()),
-        Computer(Race.Protoss, Difficulty.VeryHard)
+        Bot(Race.Zerg, ZergRushBot())
     ], realtime=False, step_time_limit=2.0, game_time_limit=(60 * 30), save_replay_as="test.SC2Replay")
 
 
